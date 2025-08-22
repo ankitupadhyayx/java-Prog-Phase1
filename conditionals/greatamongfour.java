31c90039f82564d382011a5adbcd8aa212b1c7a1
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-22 18:00:12 +0530

    greatamonfour

---
+++ conditionals/greatamongfour.java
@@ -0,0 +1,30 @@
+package conditionals;
+
+public class greatamongfour {
+    public static void main(String[] args) {
+       int a=10,b=20,c=30,d=40;
+       if(a>b){
+           if(a>c){
+               if(a>d){
+                   System.out.println(a+" : a is greater");
+               }
+           }
+       }
+       else{
+           if(b>c){
+               if(b>d){
+                   System.out.println(b +" : b is greater..");
+               }
+           }
+           else {
+               if(c>d){
+                   System.out.println(c+" : c is greater...");
+               }
+               else{
+                   System.out.println(d+" : d is greater...");
+               }
+           }
+       }
+
+    }
+}
