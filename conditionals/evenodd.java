6074e7b48b225d17b95929a216afd9d185b64b9e
Ankit Upadhyay <ankitupadhyayx@gmail.com>
2025-08-22 17:16:57 +0530

    program of even odd

---
+++ conditionals/evenodd.java
@@ -0,0 +1,14 @@
+package conditionals;
+
+public class evenodd {
+
+    public static void main(String[] args) {
+        int a = 10, b = 11;
+        if (a > b) {
+            System.out.println(a + "a is greater than b");
+        } else {
+            System.out.println(b + " :b is greater then a");
+        }
+
+    }
+}
