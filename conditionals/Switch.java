f9d189446f0854787f8508e0b9d8ed634613ec0c
Ankit upadhyay <ankitupadhyayx@gmail.com>
2025-09-23 14:45:19 +0530

    Add switch case for item pricing
    Implement a switch case to display item prices

---
+++ conditionals/Switch.java
@@ -0,0 +1,18 @@
+//switch case
+class Main {
+    public static void main(String[] args) {
+        int item = 1 ;
+        final int BURGER = 1;
+        final int PIZZA = 2 ;
+        switch(item){
+            case 1 :
+            System.out.println("Price is 100");
+            break;
+            case 2:
+            System.out.println("Price is 200");
+            break;
+            default:
+            System.out.println("50 % of on all items");
+        }
+    }
+}
